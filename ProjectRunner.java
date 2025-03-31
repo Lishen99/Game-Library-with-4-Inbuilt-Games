2424149adcb056ddd47fc8921e951de9c757b466
Lishen99 <lishen.madusha@gmail.com>
2025-03-31 15:22:33 -0400

    Refactor ProjectRunner: Remove unused UIManager settings for cleaner code

--- ProjectRunner.java
+++ ProjectRunner.java
@@ -1,5 +1,4 @@
 import javax.swing.*;
-import java.awt.*;
 
 public class ProjectRunner {
     
@@ -13,15 +12,6 @@ public class ProjectRunner {
                 // Set Nimbus Look & Feel once before any frame is created.
                 UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
                 
-                // Set global UIManager defaults.
-                UIManager.put("OptionPane.background", new Color(30, 30, 30));
-                UIManager.put("Panel.background", new Color(30, 30, 30));
-                UIManager.put("OptionPane.messageForeground", Color.WHITE);
-                UIManager.put("OptionPane.messageFont", new Font("Arial", Font.BOLD, 18));
-                UIManager.put("Button.background", new Color(30, 30, 30));
-                UIManager.put("OptionPane.buttonFont", new Font("Arial", Font.PLAIN, 14));
-                UIManager.put("ComboBox.background", new Color(30, 30, 30));
-                UIManager.put("ComboBox.selectionBackground", new Color(50, 50, 50));
             } catch (Exception e) {
                 e.printStackTrace();
             }
